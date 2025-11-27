f5715096ec8f5f35fa015d80ac99e53970605d46
HatsuneKareshi <m1e2a3n4n5@gmail.com>
2025-11-27 18:01:52 +0700

    feat: question 2 completed, not yet in project directory. to be added to prj dir

--- bigint_module/main.cpp
+++ bigint_module/main.cpp
@@ -1,35 +1,17 @@
 #include "bigint.h"
 #include <chrono>
-#include <vector> // yes this is allowed;
 
 // NOTE TO ALL: THIS main.cpp FILE IS NOT TO BE SUBMITTED AS THE FINAL PAYLOAD FOR ANY TASKS
 //              THIS FILE IS ONLY TO TEST THE bigint CLASS
 
 int main()
 {
-    std::cout << "paste input file content here\n";
-    bigint p, g, n;
-    std::vector<bigint> prime_factors{};
-    std::cin >> p;
-    std::cin >> n;
-    for (bigint i(0); i < n; i += 1)
-    {
-        bigint tmp_prime;
-        std::cin >> tmp_prime;
-        prime_factors.push_back(tmp_prime);
-    }
-    std::cin >> g;
-
-    bool answer = true;
-
-    for (auto pf : prime_factors)
-    {
-        if (powMod(g, (p - bigint(1)) / pf, p) == 1)
-        {
-            answer = false;
-            break;
-        }
-    }
-    std::cout << ((answer) ? "yes\n1" : "no\n0") << "\n";
+    std::cout << "paste input file content here, question 2\n";
+    bigint p, g, a, b; // A for alex, B for bob;
+    std::cin >> p >> g >> a >> b;
+    std::cout << "output:\n";
+    std::cout << powMod(g, a, p) << "\n"
+              << powMod(g, b, p) << "\n"
+              << powMod(powMod(g, a, p), b, p) << "\n";
     return 0;
 }
