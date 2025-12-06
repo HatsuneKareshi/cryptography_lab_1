5e92852fb805f47d3386a059250caa51e44e48bc
HatsuneKareshi <m1e2a3n4n5@gmail.com>
2025-12-06 10:56:02 +0700

    feat: question 4, completed, both in test main.cpp and in project repository. tested against available in/outputs

--- bigint_module/main.cpp
+++ bigint_module/main.cpp
@@ -4,14 +4,24 @@
 // NOTE TO ALL: THIS main.cpp FILE IS NOT TO BE SUBMITTED AS THE FINAL PAYLOAD FOR ANY TASKS
 //              THIS FILE IS ONLY TO TEST THE bigint CLASS
 
+// now this is for question 4
+
 int main()
 {
-    std::cout << "paste input file content here, question 2\n";
-    bigint p, g, a, b; // A for alex, B for bob;
-    std::cin >> p >> g >> a >> b;
-    std::cout << "output:\n";
-    std::cout << powMod(g, a, p) << "\n"
-              << powMod(g, b, p) << "\n"
-              << powMod(powMod(g, a, p), b, p) << "\n";
+    std::cout << "paste input file content here, question 4\n";
+    bigint p, g, y, m, r, h;
+    std::cin >> p >> g >> y >> m >> r >> h;
+    bool chk1 = (bigint(0) < r && r < p) && (bigint(0) < h && h < p - bigint(1));
+
+    bigint g_m = powMod(g, m, p);
+    bigint y_r = powMod(y, r, p);
+    bigint r_h = powMod(r, h, p);
+    // prettyprint_bigint(g_m);
+    // prettyprint_bigint(y_r);
+    // prettyprint_bigint(r_h);
+    // prettyprint_bigint(mulMod(y_r, r_h, p));
+
+    bool chk2 = (g_m == mulMod(y_r, r_h, p));
+    std::cout << (chk1 && chk2);
     return 0;
 }
