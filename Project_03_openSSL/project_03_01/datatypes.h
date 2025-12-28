341da8788e055d51586d068c522e8cecf5a89b81
HatsuneKareshi <m1e2a3n4n5@gmail.com>
2025-12-28 17:15:43 +0700

    feat/misc/etc: updated a bunch of util stuff, added task 2 to its directory, properly tested, compat with openSSL. task 1 redone slightly

--- Project_03_openSSL/project_03_01/datatypes.h
+++ Project_03_openSSL/project_03_01/datatypes.h
@@ -1,11 +1,14 @@
+#pragma once
 #include "bigint.h"
 
 struct publicKey
 {
     bigint N, e;
+    int NBytecount;
 };
 
 struct privatKey
 {
     bigint N, p, q, d, e;
+    int NBytecount;
 };
--- Project_03_openSSL/project_03_01/main.cpp
+++ Project_03_openSSL/project_03_01/main.cpp
@@ -1,55 +1,52 @@
 #include "openssl_rsa_handler.h"
-#include <chrono>
+#include "base64_parser.h"
+#include <cstring>
 
-int main(int argc, char **argv)
+// experimental implementation for task 2. expect more modifications to come later
+
+int main(int argc, char **argv) // main <keyfile> -enc/dec <source> <destination>
 {
     if (argc != 2)
     {
-        std::cout << "wrong arguments. syntax: .exe <key-file-name>";
+        std::cout << "incorrect arguments. syntax: .exe <key-file-name>\n";
         return -1;
     }
-
-    std::ifstream openssl(argv[1]);
-    if (openssl.is_open() != true)
+    std::cout << "Note: all integers printed left to right, most significant to least significant.\neach block of hex within bars || represent 64 bits of data.\n";
+    bool file_reported = false;
+    try
     {
-        std::cout << "key file inaccessible\n";
-        return -1;
+
+        publicKey plk = parsePubkeyFile(argv[1]);
+        std::cout << "=====PUBLIC KEY=====\n";
+        std::cout << "modulus length: " << plk.NBytecount << " bytes\n";
+        std::cout << "N= " << plk.N.debugstring() << "\n";
+        std::cout << "e= " << plk.e.debugstring() << "\n";
+        std::cout << "=====PUBLIC KEY=====\n";
+        file_reported = true;
+    }
+    catch (std::exception e)
+    {
+    }
+    try // very janky yes
+    {
+
+        privatKey pvk = parsePrvkeyFile(argv[1]);
+        std::cout << "=====PRIVATE KEY=====\n";
+        std::cout << "modulus length: " << pvk.NBytecount << " bytes\n";
+        std::cout << "N= " << pvk.N.debugstring() << "\n";
+        std::cout << "e= " << pvk.e.debugstring() << "\n";
+        std::cout << "d= " << pvk.d.debugstring() << "\n";
+        std::cout << "p= " << pvk.p.debugstring() << "\n";
+        std::cout << "q= " << pvk.q.debugstring() << "\n";
+        std::cout << "=====PRIVATE KEY=====\n";
+        file_reported = true;
+    }
+    catch (std::exception e)
+    {
     }
 
-    std::string header{};
-    std::string base64_payload = "";
+    if (!file_reported)
+        std::cout << "file inaccessble or not a key file!\n";
 
-    std::getline(openssl, header, '\n'); // read header
-
-    while (!openssl.eof()) // get the rest of the payload
-    {
-        std::string tmp{};
-        std::getline(openssl, tmp, '\n');
-        if (tmp != PUBLKEY_ENDER && tmp != PRIVKEY_ENDER)
-            base64_payload += tmp;
-        else
-            break;
-    }
-    if (header == PUBLKEY_HEADER)
-    {
-        std::cout << "public key detected\nrelevant information:\n";
-        publicKey plk = __getpublicKey(base64_payload);
-        std::cout << "N: " << plk.N.debugstring() << "\n";
-        std::cout << "e: " << plk.e.debugstring() << "\n";
-    }
-
-    else if (header == PRIVKEY_HEADER)
-    {
-        std::cout << "private key detected\nrelevant information:\n";
-        privatKey pvk = __getprivatKey(base64_payload);
-        std::cout << "N: " << pvk.N.debugstring() << "\n";
-        std::cout << "e: " << pvk.e.debugstring() << "\n";
-        std::cout << "d: " << pvk.d.debugstring() << "\n";
-        std::cout << "p: " << pvk.p.debugstring() << "\n";
-        std::cout << "q: " << pvk.q.debugstring() << "\n";
-        std::cout << "p's primality: " << (rabin_miller(pvk.p, 5) ? "true" : "false") << "\n";
-        std::cout << "q's primality: " << (rabin_miller(pvk.q, 5) ? "true" : "false") << "\n";
-        std::cout << "verifying N == p * q: " << (pvk.N == pvk.p * pvk.q ? "true" : "false") << "\n";
-    }
-    openssl.close();
+    return 0;
 }
