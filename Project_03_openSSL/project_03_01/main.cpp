89e3e19726adcdc9915a7d969ca2f1b5411f054e
HatsuneKareshi <m1e2a3n4n5@gmail.com>
2026-01-02 12:56:45 +0700

    misc: updated module to task 1, changed the main of task 1 to take an additional argument

--- Project_03_openSSL/project_03_01/main.cpp
+++ Project_03_openSSL/project_03_01/main.cpp
@@ -4,49 +4,57 @@
 
 // experimental implementation for task 2. expect more modifications to come later
 
-int main(int argc, char **argv) // main <keyfile> -enc/dec <source> <destination>
+int main(int argc, char **argv) // main <keyfile> -pub/priv
 {
-    if (argc != 2)
+    if (argc != 3)
     {
-        std::cout << "incorrect arguments. syntax: .exe <key-file-name>\n";
+        std::cout << "incorrect arguments. syntax: \n.exe <key-file-name> -pub\n                     -priv\n";
         return -1;
     }
     std::cout << "Note: all integers printed left to right, most significant to least significant.\neach block of hex within bars || represent 64 bits of data.\n";
-    bool file_reported = false;
-    try
+    if (strcmp(argv[2], "-pub") && strcmp(argv[2], "-priv"))
     {
-
-        publicKey plk = parsePubkeyFile(argv[1]);
-        std::cout << "=====PUBLIC KEY=====\n";
-        std::cout << "modulus length: " << plk.NBytecount << " bytes\n";
-        std::cout << "N= " << plk.N.debugstring() << "\n";
-        std::cout << "e= " << plk.e.debugstring() << "\n";
-        std::cout << "=====PUBLIC KEY=====\n";
-        file_reported = true;
+        std::cout << "incorrect syntax. action specifier is in position 2, and is either -pub or -priv\n";
+        return -1;
     }
-    catch (std::exception e)
+    if (!strcmp(argv[2], "-pub"))
     {
+        try
+        {
+
+            publicKey plk = parsePubkeyFile(argv[1]);
+            std::cout << "=====PUBLIC KEY=====\n";
+            std::cout << "modulus length: " << plk.NBytecount << " bytes\n";
+            std::cout << "N= " << plk.N.debugstring() << "\n";
+            std::cout << "e= " << plk.e.debugstring() << "\n";
+            std::cout << "=====PUBLIC KEY=====\n";
+        }
+        catch (const std::exception &e)
+        {
+            std::cout << "error parsing public key, " << e.what() << "\n";
+            return -1;
+        }
     }
-    try // very janky yes
+    if (!strcmp(argv[2], "-priv"))
     {
+        try
+        {
 
-        privatKey pvk = parsePrvkeyFile(argv[1]);
-        std::cout << "=====PRIVATE KEY=====\n";
-        std::cout << "modulus length: " << pvk.NBytecount << " bytes\n";
-        std::cout << "N= " << pvk.N.debugstring() << "\n";
-        std::cout << "e= " << pvk.e.debugstring() << "\n";
-        std::cout << "d= " << pvk.d.debugstring() << "\n";
-        std::cout << "p= " << pvk.p.debugstring() << "\n";
-        std::cout << "q= " << pvk.q.debugstring() << "\n";
-        std::cout << "=====PRIVATE KEY=====\n";
-        file_reported = true;
+            privatKey pvk = parsePrvkeyFile(argv[1]);
+            std::cout << "=====PRIVATE KEY=====\n";
+            std::cout << "modulus length: " << pvk.NBytecount << " bytes\n";
+            std::cout << "N= " << pvk.N.debugstring() << "\n";
+            std::cout << "e= " << pvk.e.debugstring() << "\n";
+            std::cout << "d= " << pvk.d.debugstring() << "\n";
+            std::cout << "p= " << pvk.p.debugstring() << "\n";
+            std::cout << "q= " << pvk.q.debugstring() << "\n";
+            std::cout << "=====PRIVATE KEY=====\n";
+        }
+        catch (const std::exception &e)
+        {
+            std::cout << "error parsing public key, " << e.what() << "\n";
+            return -1;
+        }
     }
-    catch (std::exception e)
-    {
-    }
-
-    if (!file_reported)
-        std::cout << "file inaccessble or not a key file!\n";
-
     return 0;
 }
